f1655e6801ae47b7f2837459d60df463320f37c0
Valentijn Muijrers <vmuijrers@gmail.com>
2018-12-12 10:41:09 +0100

    Update

---
+++ Death's Compass/Assets/Scripts/OrbSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrbSpawner : MonoBehaviour {
+
+    public Transform ObjecToFollow;
+    public float maxTimer;
+
+    public float spawnDistanceFromPlayer;
+    public LayerMask layerMask;
+    public AudioClip[] spawnSounds;
+    public AudioClip[] pickupSounds;
+    public Text text;
+    int score = 0;
+    private float currentTimer = 0;
+	// Use this for initialization
+	void Start () {
+        Respawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTimer -= Time.deltaTime;
+        if(currentTimer <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(pickupSounds[Random.Range(0,pickupSounds.Length)], transform.position);
+            Debug.Log("Pickedup!");
+            score++;
+            UpdateScore();
+            Respawn();
+        }
+        
+    }
+    void UpdateScore()
+    {
+        text.text = "Score: " + score.ToString();
+    }
+
+    void Respawn()
+    {
+        currentTimer = maxTimer;
+        Vector3 rayCastPoint=ObjecToFollow.transform.position + (new Vector3(Random.Range(-1,1),0,Random.Range(-1,1))).normalized * spawnDistanceFromPlayer + new Vector3(0,40,0);
+        RaycastHit hit;
+        if (Physics.Raycast(rayCastPoint, -Vector3.up,out hit,100, layerMask))
+        {
+            AudioSource.PlayClipAtPoint(spawnSounds[Random.Range(0, pickupSounds.Length)], transform.position);
+            transform.position = hit.point;
+        }
+        else
+        {
+            Respawn();
+        }
+    }
+}
